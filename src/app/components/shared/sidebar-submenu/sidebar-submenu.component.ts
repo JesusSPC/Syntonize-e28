1ccb9ceaf1b05a6d59263d2826cacebb4a8109af
Jesús <Jesus.Saints@gmail.com>
2020-05-06 10:14:21 +0200

    Added sidebar-submenu component

--- src/app/components/shared/sidebar-submenu/sidebar-submenu.component.ts
+++ src/app/components/shared/sidebar-submenu/sidebar-submenu.component.ts
@@ -1,28 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter, HostBinding, AfterViewInit, ViewChild, ContentChildren, AfterContentInit, QueryList, Renderer2 } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'sidebar-submenu',
   templateUrl: './sidebar-submenu.component.html',
   styleUrls: ['./sidebar-submenu.component.scss']
 })
-export class SidebarSubmenuComponent implements AfterContentInit {
-  @Input() link: any;
-  @ViewChild('submenu') submenu: any;
-  // @ContentChildren('submenuItem') sublinks: QueryList<any>;
+export class SidebarSubmenuComponent {
+  @Input() menu: any;
 
 
-  constructor(
-    private renderer: Renderer2
-  ) {}
+  constructor() {}
 
-  ngAfterContentInit() {
-    // this.sublinks.toArray().forEach(sublink =>
-    //   this.renderer.setStyle(sublink.nativeElement, 'transform', `translateY(${-100 * this.sublinks.length}%)`));
-  }
 
-  // toggleSubmenuTranslateY() {
-  //   const action = this.link.active ? 'addClass' : 'removeClass';
-  //   console.log(this.sublinks)
-  //   this.sublinks.toArray().forEach(sublink => this.renderer[action](sublink.nativeElement, 'sub-list-open'));
-  // }
 }
