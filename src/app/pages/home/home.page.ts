c3002cb9ed27dbbef4814e7548ed57fe76c6b1d2
Jesús <Jesus.Saints@gmail.com>
2020-05-19 13:26:36 +0200

    bug: retrieving data from API displays last request

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { PunkService } from 'src/app/services';
-import { Observable } from 'rxjs';
+import { Observable, merge, concat } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'home-page',
@@ -12,15 +13,25 @@ export class HomePage implements OnInit {
   beersPagination: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   beersListPage = 1;
   beersListLimit = 5;
+  allBeers = [];
 
   constructor(private punkService: PunkService) { }
 
   ngOnInit(): void {
-    this.beers$ = this.punkService.getBeers(this.beersListPage, this.beersListLimit);
+    const request$ = this.punkService.getAllBeers(this.beersListPage, 80);
+    this.beers$ = concat(...request$);
+  }
+
+  ngAfterViewInit() {
+    // this.beers$.subscribe(observer => console.log(observer));
   }
 
   setBeersList(listNumber: number) {
     this.beersListPage = listNumber;
     this.beers$ = this.punkService.getBeers(this.beersListPage, this.beersListLimit);
+  }
+
+  ngOnDestroy() {
+
   }
 }
--- src/app/services/punk.service.ts
+++ src/app/services/punk.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class PunkService {
   url = 'https://api.punkapi.com/v2/';
   constructor(private http: HttpClient) {}
+
+  getAllBeers(listNumber, listLimit) {
+    const allBeers: Observable<{}>[] = [];
+    const endLoop = 3;
+    for (let i = listNumber; i < endLoop; i++) {
+      const request$ = this.http.get(`${this.url}beers?page=${i}&per_page=${listLimit}`);
+      allBeers.push(request$);
+    }
+    return allBeers;
+  }
 
   getBeers(listNumber: number, listLimit: number) {
     return this.http.get(`${this.url}beers?page=${listNumber}&per_page=${listLimit}`);
